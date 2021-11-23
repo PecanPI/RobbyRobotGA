ee6ee0e55c0562b378afaf3af494ae738204fb04
Matthew Logan <mloga407@gmail.com>
2021-11-22 17:57:23 -0700

    runs multiple robots on multiple boards

--- game.py
+++ game.py
@@ -17,91 +17,92 @@ WIN_WIDTH = (WIDTH + MARGIN) * SIZE
 
 
 class Robby:
-    def __init__(self, x, y):
+    def __init__(self, x, y, board):
         self.x = x
         self.y = y
         self.size = 10
+        self.moves = 200
+        self.board = board
         self.fitness = 0
-        self.genome = str(np.random.randint(7, size=(243)))
+        self.total_fitness = 0
+        self.genome = np.random.randint(7, size=(243))
         self.color = BLUE
 
-    def move(self, gene, board):
-        print(f"gene: {gene}, type: {type(gene)}")
-        if gene == '0':
+    def move(self, gene):
+        if gene == 0:
             if self.y - 1 > 0:
                 self.y -= 1 
             else:
                 self.fitness -= 5
-        elif gene == '1':
+        elif gene == 1:
             if self.y  + 1 < 10:
                 self.y += 1 
             else:
                 self.fitness -= 5
            
-        elif gene == '2':
+        elif gene == 2:
             if self.x -  1 > 0:
                 self.x -= 1 
             else:
                 self.fitness -= 5
         
-        elif gene == '3':
+        elif gene == 3:
             if self.x  + 1 < 10:
                 self.x += 1 
             else:
                 self.fitness -= 5
 
-        elif gene == '4':
+        elif gene == 4:
             pass # do nothing
-        elif gene == '5':
+        elif gene == 5:
             #pick up
-            if board[self.x][self.y] == 1:
-                 board[self.x][self.y] = 0
+            if self.board[self.x][self.y] == 1:
+                 self.board[self.x][self.y] = 0
             else:
                 self.fitness -= 1
-        elif gene == '6':
+        elif gene == 6:
             #move random direction
-            self.move(np.random.randint(0,5), board)
+            self.move(np.random.randint(0,5))
+        self.moves -= 1
 
-    def find_move(self, board):
+    def find_move(self):
         # up * 3^0 + down * 3^1 + left *3^2 + right * 3^3 + cur * 3^4 = index of gene
         #empty = 0, can = 1, wall = 2
         #up:
         if self.y - 1 < 0:
             up = 2
-        elif board[self.x][self.y - 1] == 1:
+        elif self.board[self.x][self.y - 1] == 1:
             up = 1
         else:
             up = 0
         #down:
         if self.y + 1 > 9:
             down = 2
-        elif board[self.x][self.y + 1] == 1:
+        elif self.board[self.x][self.y + 1] == 1:
             down = 1
         else:
             down = 0
         #left:
         if self.x - 1 < 0:
             left = 2
-        elif board[self.x - 1][self.y] == 1:
+        elif self.board[self.x - 1][self.y] == 1:
             left = 1
         else:
             left = 0
         #right:
-        if self.y + 1 > 9:
+        if self.x + 1 > 9:
             right = 2
-        elif board[self.x + 1][self.y] == 1:
+        elif self.board[self.x + 1][self.y] == 1:
             right = 1
         else:
             right = 0
         #current, cant be a wall so we ignore:
-        if board[self.x][self.y - 1] == 1:
+        if self.board[self.x][self.y - 1] == 1:
             cur = 1
         else:
             cur = 0
 
         move = int(down * pow(3,0) + up * pow(3,1) + left * pow(3,2) + right * pow(3,3) + cur * pow(3,4))
-        print(move)
-        print(self.genome[move])
         return self.genome[move]
 
     def draw(self, win):
@@ -110,17 +111,22 @@ class Robby:
                          self.size, self.size))
 
 
-def create_board(size):
-    grid = np.empty(size*size)
-    # Create grid of square size and randomly place cans over board
-    for i in range(size*size):
-        if i < 50:
-            grid[i] = 0
-        else:
-            grid[i] = 1
-    np.random.shuffle(grid)
-    grid = grid.reshape(size, size)
-    return grid
+class Board():
+
+    def __init__(self, size):
+        self.size = size
+        self.grid = self.make_grid(self.size)
+    def make_grid(self, size):
+        grid = np.empty(size*size)
+        # Create grid of square size and randomly place cans over board
+        for i in range(size*size):
+            if i < 50:
+                grid[i] = 0
+            else:
+                grid[i] = 1
+        np.random.shuffle(grid)
+        grid = grid.reshape(size, size)
+        return grid
 
 
 def draw(win, board, rob):
@@ -145,19 +151,41 @@ pygame.display.set_caption('Robby')
 run = True
 
 
-board = create_board(SIZE)
-print(board)
 clock = pygame.time.Clock()
-rob = Robby(5, 5)
-print(rob.genome)
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    rob.move(rob.find_move(board), board)
-    draw(win, board, rob)
-    clock.tick(1)
-    
+population_size = 20
+cleaning_sessions= 5
+boards = [Board(SIZE) for _ in range(cleaning_sessions)]
+robots = [Robby(0,0, boards[0].grid) for _ in range(population_size)]
+
+
+
+for rob in robots:
+    for board in boards:
+        rob.board = board.grid
+        rob.moves = 200
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+            print(rob.moves)
+            if rob.moves > 0:
+                rob.move(rob.find_move())
+                draw(win, board.grid, rob)
+            else:
+                run = False
+        rob.total_fitness += rob.fitness
+        rob.fitness = 0
+    rob.total_fitness /= population_size
+        
+            # clock.tick(30)
+                        
+
+# print(rob.moves)
+print("All fitness")
+robots = sorted(robots, key=lambda x:x.total_fitness, reverse=True)
+for rob in robots:
+    print(rob.total_fitness)
 
 pygame.quit()
